5b7b2d03dad64bf747ad5fb3323696d38b7dfcae
Beyza Nur Çetiner <190101036@ogrenci.yalova.edu.tr>
2022-01-26 17:37:38 +0300

    Java camp day 5

---
+++ nLayeredDemo/src/nLayeredDemo/Main.java
@@ -0,0 +1,26 @@
+package nLayeredDemo;
+
+import nLayeredDemo.business.abstracts.ProductService;
+import nLayeredDemo.business.concretes.ProductManager;
+import nLayeredDemo.core.JLoggerManagerAdapter;
+import nLayeredDemo.dataAccess.concretes.HibernateProductDao;
+import nLayeredDemo.entities.concretes.Product;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//toDo: Spring IoC ile cozulecek 
+	
+		
+		ProductService productService=new ProductManager(new HibernateProductDao() , new JLoggerManagerAdapter());
+		
+		Product product=new Product(1,2,"elma",12,50);
+		productService.add(product);
+		
+		
+		
+
+	}
+
+}
